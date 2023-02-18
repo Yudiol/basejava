67829439c7f97636d42fe8ba6d1174d2436984c2
Roman Yudin <yudiol114@gmail.com>
2023-02-18 14:48:56 +0300

    Entrance exam

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,73 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
+    {
+        Arrays.fill(storage, null);
+    }
+
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i] == r) {
+                break;
+            }
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
+
     }
 
     Resume get(String uuid) {
+        getAll();
+        for (Resume resume : storage) {
+            if (resume != null && Objects.equals(resume.uuid, uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int mark =-1;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null && Objects.equals(storage[i].uuid, uuid)) {
+                storage[i] = null;
+                mark = i;
+            }
+            if (mark != -1) {
+                storage[i] = storage[i + 1];
+            }
+            if (i == storage.length - 2) {
+                storage[i + 1] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        int counter = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                counter++;
+            }
+        }
+        return counter;
     }
 }

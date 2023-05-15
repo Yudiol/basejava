971e2f56f3d2a43a49a2c62e208a5d68c5787121
Roman Yudin <yudiol114@gmail.com>
2023-05-16 00:08:24 +0300

    HW12

--- src/com/urise/webapp/HW12.java
+++ src/com/urise/webapp/HW12.java
@@ -6,13 +6,16 @@ import java.util.stream.Collectors;
 
 public class HW12 {
     public static void main(String[] args) {
-        System.out.println(minValue(new int[]{6, 1, 2, 3, 4, 5, 5}));
-        System.out.println(minValueConvertToString(new int[]{9, 8}));
-        System.out.println(oddOrEven(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 1)));
+        System.out.println(minValueMySolution(new int[]{6, 1, 2, 3, 4, 5, 5}));
+        System.out.println(minValueAnotherManSolution(new int[]{6, 1, 2, 3, 4, 5, 5}));
+        System.out.println(minValueConvertToString(new int[]{9, 8, 8, 9}));
+        System.out.println(oddOrEven(Arrays.asList(1, 1,1, 2, 2)));
     }
 
-    static int minValue(int[] values) {
-        int[] arr = Arrays.stream(values).distinct().sorted().toArray();
+    static int minValueMySolution(int[] values) {
+        int[] arr = Arrays.stream(values).distinct()
+                .sorted()
+                .toArray();
         int result = 0;
         for (int number : arr) {
             result = 10 * result + number;
@@ -20,15 +23,26 @@ public class HW12 {
         return result;
     }
 
+    static int minValueAnotherManSolution(int[] values) {
+        return Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce((a, e) -> a * 10 + e)
+                .orElse(0);
+    }
+
     static int minValueConvertToString(int[] values) {
-        String number = Arrays.stream(values).distinct().sorted().mapToObj(e -> e + "").collect(Collectors.joining());
-        return Integer.parseInt(number);
+        return Integer.parseInt(Arrays.stream(values).distinct().sorted().mapToObj(e -> e + "").collect(Collectors.joining()));
     }
 
     static List<Integer> oddOrEven(List<Integer> integers) {
-        Integer number = integers.stream().reduce(Integer::sum).orElse(0);
-        return number % 2 == 0 ? integers.stream().filter(e -> e % 2 != 0).collect(Collectors.toList()) :
-                integers.stream().filter(e -> e % 2 == 0).collect(Collectors.toList());
+//        boolean b = integers.stream().reduce(Integer::sum).map(e -> e % 2 == 0).orElse(false);
+//        return integers.stream().collect(Collectors.groupingBy(el -> el % 2 == 0)).get(!b);
+        return integers.stream()
+                .collect(Collectors.collectingAndThen(Collectors.toList(),
+                        e -> {
+                            return e.stream().collect(Collectors.groupingBy(el -> el % 2 == 1))
+                                    .get(e.stream().reduce(Integer::sum).orElse(0) % 2 == 0);
+                        }));
     }
-
 }
